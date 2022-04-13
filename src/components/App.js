fc463ff9d794c62aa0495c018e783414fd8ecea1
CrackerJaps <thecrackerjaps@gmail.com>
2022-04-12 21:50:18 -0700

    Initial commit - basic webpack/babel boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>Hello World</h1>
+      <p>text text text text</p>
+    </div>
+  )
+}
